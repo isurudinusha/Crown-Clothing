f9d37206cbcd0c30e439df407e70cf862c695d5e
Isuru Dinusha <isurudinusha@gmail.com>
2024-06-16 01:25:03 +0530

    chore: Update Firebase configuration in firebase.utils.js

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -6,14 +6,7 @@ import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-    apiKey: "AIzaSyAsbk2pfqZ0SddKiXdYi6sTH8NC0f2L7bQ",
-    authDomain: "crown-clothing-b53ff.firebaseapp.com",
-    projectId: "crown-clothing-b53ff",
-    storageBucket: "crown-clothing-b53ff.appspot.com",
-    messagingSenderId: "1049656970004",
-    appId: "1:1049656970004:web:4e2ab4f7a85646706f5001"
-};
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
